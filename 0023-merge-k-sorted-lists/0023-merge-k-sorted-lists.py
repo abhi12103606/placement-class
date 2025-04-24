69d48f7640347724695ff15e25849cf2f99d5ab2
abhi12103606 <103558199+abhi12103606@users.noreply.github.com>
2025-04-24 14:53:42 +0530

    Time: 5 ms (94.32%), Space: 20.4 MB (36.11%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,25 +1,13 @@
-#include<bits/stdc++.h>
-class Solution {
-public:
-    int romanToInt(string s) {
-        map<char,int>m1;
-        m1['I'] = 1;
-        m1['V'] = 5;
-        m1['X'] = 10;
-        m1['L'] = 50;
-        m1['C'] = 100;
-        m1['D'] = 500;
-        m1['M'] = 1000;
-        int sum =0;
-        for(int i=0;i<s.length();i++){
-            char c1=s[i];
-            if(i<s.length()-1 && m1[c1]<m1[s[i+1]]){
-                sum=sum+(m1[s[i+1]]-m1[c1]);
-                i++;
-            }else{
-                sum=sum+m1[c1];
-            }
-        }
-        return sum;
-    }
-};
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        v=[]
+        for i in lists:
+            x=i
+            while x:
+                v+=[x.val]
+                x=x.next
+        v=sorted(v,reverse=True)
+        ans=None
+        for i in v:
+            ans=ListNode(i,ans)
+        return ans
