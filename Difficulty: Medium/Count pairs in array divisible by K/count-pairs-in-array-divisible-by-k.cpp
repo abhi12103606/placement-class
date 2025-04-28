0f71a146981808aeb7303733aa5cf3f08de05792
abhi12103606 <103558199+abhi12103606@users.noreply.github.com>
2025-04-28 11:02:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count pairs in array divisible by K/count-pairs-in-array-divisible-by-k.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+    long long countKdivPairs(int A[], int n, int K)
+    {
+        vector<int> freq(K, 0);
+        for(int i=0; i<n; i++){
+            int ele = A[i]%K;
+            freq[ele]++;
+        }
+        
+        long long ans=0;
+        for(int i=0; i<n; i++){
+            int ele = A[i]%K;
+            freq[ele]--;
+            
+            int target = K-ele;
+            if(ele!=0){
+                ans+=freq[target];
+            }
+            else{
+                ans+=freq[0];
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+
+		int a[n];
+		for (int i = 0; i < n; i++)
+			cin >> a[i];
+
+		int k;
+		cin >> k;
+
+        Solution ob;
+		cout << ob. countKdivPairs(a, n , k) << "\n";
+
+
+
+	
+cout << "~" << "\n";
+}
+
+
+	return 0;
+}
+
+// } Driver Code Ends
