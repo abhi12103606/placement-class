42ee4d0b644a46951d7601cf6e1cf86cd2b2e839
abhi12103606 <103558199+abhi12103606@users.noreply.github.com>
2025-04-28 13:32:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count pairs in array divisible by K/count-pairs-in-array-divisible-by-k.cpp
+++ Difficulty: Medium/Count pairs in array divisible by K/count-pairs-in-array-divisible-by-k.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 // } Driver Code Ends
 
+
 // User function Template for C++
 
 class Solution {
@@ -35,6 +36,8 @@ class Solution {
         return ans;
     }
 };
+
+
 
 
 //{ Driver Code Starts.
