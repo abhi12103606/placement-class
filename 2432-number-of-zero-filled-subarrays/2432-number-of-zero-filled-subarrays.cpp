c3bfdc0edae81ec2ada24f0615e71b5d53825b58
abhi12103606 <103558199+abhi12103606@users.noreply.github.com>
2025-04-28 14:01:36 +0530

    Time: 12 ms (6.47%), Space: 111.3 MB (73.94%) - LeetHub

---
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long contCount = 0;
+        long long subarrays = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                contCount++;
+            }
+            else{
+                subarrays += (contCount*(contCount+1)/2);
+                contCount=0;
+            }
+        }
+        // if the last element was 0, we need to calculate for it
+        if(contCount>0){
+            subarrays += (contCount*(contCount+1)/2);
+        }
+        return subarrays;
+    }
+};
